e2a6aace7e046c31a886d8b0a91e7995d8cb247c
xingtingbiao <xingtingbiao@znlhzl.com>
2019-02-27 12:02:31 +0800

    feature: 有状态的词频统计

--- notes/第7章 Spark-Streaming进阶/Spark Streaming进阶.md
+++ notes/第7章 Spark-Streaming进阶/Spark Streaming进阶.md
@@ -10,7 +10,15 @@ Spark Streaming进阶
 
 
 1) 带状态的算子: UpdateStateByKey
+The updateStateByKey operation allows you to maintain arbitrary state while continuously updating it with new information. To use this, you will have to do two steps.
 
+    Define the state - The state can be an arbitrary data type.
+    Define the state update function - Specify with a function how to update the state using the previous state and the new values from an input stream.
+
+需求: 统计到目前为止累计出现的单词个数(需要保持住以前的状态)
+详见代码: com.xtb.spark.streaming.StatefulWordCount
+requirement failed: The checkpoint directory has not been set. Please set it by StreamingContext.checkpoint().
+如果使用stateful的算子, 必须要设置checkpoint
 
 
 
---
+++ sparktrain/src/main/scala/com/xtb/spark/streaming/StatefulWordCount.scala
@@ -0,0 +1,35 @@
+package com.xtb.spark.streaming
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * 有状态的词频统计
+  */
+object StatefulWordCount {
+  Logger.getLogger("org").setLevel(Level.WARN)
+
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setMaster("local[2]").setAppName("StatefulWordCount")
+    val ssc = new StreamingContext(sparkConf, Seconds(5))
+
+    // 如果使用stateful的算子, 必须要设置checkpoint
+    // 建议在生产环境中, 把checkpoint设置到HDFS的某个文件夹中
+    ssc.checkpoint(".")
+    val lines = ssc.socketTextStream("hadoop001", 9999)
+    val result = lines.flatMap(_.split(" ")).map((_, 1))
+    // 这里的隐式转换 _去掉貌似也可以
+    val state = result.updateStateByKey(updateFunction _)
+    state.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+  def updateFunction(currentValues: Seq[Int], preValues: Option[Int]): Option[Int] = {
+    val current = currentValues.sum
+    val pre = preValues.getOrElse(0)
+    Some(current + pre)
+  }
+}
