089aad48c0bbcbe93937912e810661adabaee430
xingtingbiao <xingtingbiao@znlhzl.com>
2019-03-12 16:53:47 +0800

    feature: spark的可视化springboot项目

---
+++ spark-web/src/main/java/com/xtb/spark/sparkweb/SparkWebApplication.java
@@ -0,0 +1,13 @@
+package com.xtb.spark.sparkweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkWebApplication.class, args);
+    }
+
+}
---
+++ spark-web/src/test/java/com/xtb/spark/sparkweb/SparkWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xtb.spark.sparkweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SparkWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
