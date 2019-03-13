f83c8f628df4082526530f36c035b5893e5d04d0
xingtingbiao <xingtingbiao@znlhzl.com>
2019-03-13 15:49:56 +0800

    feature: 动态饼图的前后台联调

--- spark-web/src/main/java/com/xtb/spark/sparkweb/controller/CourseController.java
+++ spark-web/src/main/java/com/xtb/spark/sparkweb/controller/CourseController.java
@@ -5,6 +5,7 @@ import com.xtb.spark.sparkweb.domain.CourseClickCount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,11 +20,12 @@ public class CourseController {
     @Autowired private CourseClickCountDao countDao;
 
     @RequestMapping(value = "dynamic", method = RequestMethod.GET)
-    public ModelAndView firstDemo() throws IOException {
-        String tableName = "imooc_course_clickcount";
-        String day = "20190312";
+    public ModelAndView firstDemo(@RequestParam(value = "tableName") String tableName,
+                                  @RequestParam(value = "day") String day) throws IOException {
+//        String tableName = "imooc_course_clickcount";
+//        String day = "20190312";
         List<CourseClickCount> list = countDao.query(tableName, day);
-        ModelAndView modelAndView = new ModelAndView("index", "list", list);
+        ModelAndView modelAndView = new ModelAndView("echarts", "list", list);
         List<String> names = list.stream().map(CourseClickCount::getName).collect(Collectors.toList());
         return modelAndView.addObject("names", names);
     }
